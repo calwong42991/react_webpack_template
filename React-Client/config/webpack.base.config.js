20a01bbf3107a9359ea83579be10a96c9311bd50
Calvin Wong <calwong42991@gmail.com>
2019-01-23 15:19:40 -0800

    setup webpack

--- React-Client/config/webpack.base.config.js
+++ React-Client/config/webpack.base.config.js
@@ -1,25 +1,57 @@
-const path = require('path');
-let HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // const config = {
 // }
 // config.entry = ['babel-polyfill', './src/index.jsx']
 
-module.exports = {
-  entry: './React-Client/src/index.jsx',
-  module: {
-    rules: [{
-      test: /\.jsx$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
-      }
-    }, ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './React-Client/dist/index.html',
-      filename: './index.html'
-    })
-  ]
+module.exports = env => {
+  const {
+    PLATFORM,
+    VERSION
+  } = env;
+
+  const APP_DIR = path.resolve(__dirname, '../src/index.jsx');
+
+  return merge([{
+    entry: ['@babel/polyfill', APP_DIR],
+    module: {
+      rules: [{
+          test: /\.jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader', 'sass-loader'
+          ]
+        },
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './React-Client/src/index.html',
+        filename: './index.html'
+      }),
+      new webpack.DefinePlugin({
+        'process.env.VERSION': JSON.stringify(env.VERSION),
+        'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
+      }),
+      new CopyWebpackPlugin([{
+        from: 'React-Client/src/static'
+      }]),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    ]
+  }])
 }
--- React-Client/src/Components/App.jsx
+++ React-Client/src/Components/App.jsx
@@ -2,8 +2,12 @@ import React, { Component } from 'react'
 
 class App extends Component {
   render () {
+    console.log('process.env.VERSION', process.env.VERSION);
+    console.log('process.env.PLATFORM', process.env.PLATFORM);
+    console.log('process.env.NODE_ENV', process.env.NODE_ENV);
     return (
       <div>
+        <img alt="header" src="/dist/images/header.jpg" className="app-header" />
         <h1> React Application</h1>
       </div>
     )
